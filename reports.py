d1220236f007ef66b10fcda3facff89967239117
Albert <albert@DESKTOP-N71VLB4.attlocal.net>
2023-12-28 10:06:57 -0600

    Add pie chart to report.  Displays total sales of each car made

--- reports.py
+++ reports.py
@@ -3,9 +3,14 @@
 import reportlab
 
 from reportlab.platypus import SimpleDocTemplate
-from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.platypus import Paragraph, Spacer, Table, Image 
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+from pandas import DataFrame
+import numpy as np
 
 def generate(filename, title, additional_info, table_data):
   styles = getSampleStyleSheet()
@@ -16,5 +21,34 @@ def generate(filename, title, additional_info, table_data):
                 ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
                 ('ALIGN', (0,0), (-1,-1), 'CENTER')]
   report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
+
+  # add pie chart
+  drawing = Drawing(450, 225)
+
+  report_piechart = Pie()
+  report_piechart.x = 65
+  report_piechart.y = 15
+  report_piechart.width = 140
+  report_piechart.height = 140
+
+  columns = table_data[0]
+  table_data = table_data[1:]
+  df = DataFrame(table_data,columns=columns)
+  report_piechart.data = df.loc[:,'Total Sales'].values.tolist()
+  report_piechart.labels = df.loc[:,'Car'].values.tolist()
+
+  report_piechart.slices.strokeWidth=0.5
+  report_piechart.slices[3].popout = 10
+  report_piechart.slices[3].strokeWidth = 2
+  report_piechart.slices[3].strokeDashArray = [2,2]
+  report_piechart.slices[3].labelRadius = 1.75
+  report_piechart.slices[3].fontColor = colors.red
+
+  report_piechart.sideLabels = True
+
+  drawing.add(report_piechart)
+
+  # add bar chart
+
   empty_line = Spacer(1,20)
-  report.build([report_title, empty_line, report_info, empty_line, report_table])
+  report.build([report_title, empty_line, report_info, empty_line, report_table, drawing])
