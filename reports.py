af6e75f233c35d1800809486767a88060a8352b6
Albert <albert@DESKTOP-N71VLB4.attlocal.net>
2023-12-29 10:19:08 -0600

    Add bar chart to report.  Displays total revenue of top 10 best-selling vehicles

--- reports.py
+++ reports.py
@@ -9,8 +9,11 @@ from reportlab.lib import colors
 
 from reportlab.graphics.shapes import Drawing
 from reportlab.graphics.charts.piecharts import Pie
+from reportlab.graphics.charts.barcharts import VerticalBarChart
 from pandas import DataFrame
 import numpy as np
+
+import re
 
 def generate(filename, title, additional_info, table_data):
   styles = getSampleStyleSheet()
@@ -22,14 +25,14 @@ def generate(filename, title, additional_info, table_data):
                 ('ALIGN', (0,0), (-1,-1), 'CENTER')]
   report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
 
-  # add pie chart
-  drawing = Drawing(450, 225)
+# add pie chart
+  pie_drawing = Drawing(300, 175)
 
   report_piechart = Pie()
   report_piechart.x = 65
   report_piechart.y = 15
-  report_piechart.width = 140
-  report_piechart.height = 140
+  report_piechart.width = 120
+  report_piechart.height = 120
 
   columns = table_data[0]
   table_data = table_data[1:]
@@ -46,9 +49,51 @@ def generate(filename, title, additional_info, table_data):
 
   report_piechart.sideLabels = True
 
-  drawing.add(report_piechart)
+  pie_drawing.add(report_piechart)
 
-  # add bar chart
+# add bar chart
+  bar_drawing = Drawing(300, 175)
+
+  columns = table_data[0]
+  table_data = table_data[1:]
+
+  # calculates total revenue from car sales and price
+  df['Revenue'] = df['Price']
+  
+  # removes '$' from Price data, calculats total revenue, 
+  # and creates a new Revenue column 
+  newlist = []
+  for element in (df.loc[:,'Revenue'].values.tolist()):
+      newelem = re.sub('[$]', '', element)
+      newlist.append(float(newelem))
+  df['PriceAsFloat'] = newlist
+  df['Revenue'] = df['Total Sales'] * df['PriceAsFloat']
+  # sorts dataframe in descending order according to Revenue, resets index otherwise   # a KeyError will occur because the indexes will be out-of-order
+  sorted_df = df.sort_values(by=['Revenue'],ascending=False).reset_index(drop = True)
+  data = [
+  sorted_df.loc[0:10,'Revenue'].values.tolist(), ] 
+
+  bc=VerticalBarChart()
+  bc.x = 65  # horizontal position
+  bc.y = 15  # vertical position 
+  bc.height = 120 # height of the chart area
+  bc.width = 270  # width of the chart area
+  bc.data = data
+  bc.strokeColor = colors.black
+  bc.bars[0].fillColor=colors.yellow
+  bc.groupSpacing = 24
+  bc.barSpacing = 2
+  bc.valueAxis.valueMin = 0
+  bc.valueAxis.valueMax = 15000000
+  bc.valueAxis.valueStep = 1000000
+  bc.categoryAxis.labels.boxAnchor = 'ne'
+  bc.categoryAxis.labels.dx = -5
+  bc.categoryAxis.labels.dy = 0
+  bc.categoryAxis.labels.angle = 30
+  bc.categoryAxis.categoryNames = sorted_df.loc[0:10,'Car'].values.tolist()
+
+  bar_drawing.add(bc)
+
 
   empty_line = Spacer(1,20)
-  report.build([report_title, empty_line, report_info, empty_line, report_table, drawing])
+  report.build([report_title, empty_line, report_info, empty_line, report_table, pie_drawing, bar_drawing])
